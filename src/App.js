6b74be8aee886cefb2ca5fb9b16eca2949fee667
Marce Fuentes <62070422+marcefuentesl@users.noreply.github.com>
2021-08-31 21:19:12 -0500

    Update App.js
    Agregar ejercicio del reloj

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
 //librerias ó components
 import "./App.css";
 import "./index.css";
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   
   const [users, setUsers] = useState([{ id: 0, name: "Ruben" }]);
 
-  const onClick = () => {};  
+  const onClick = () => {
+    setUsers([...users, {id: users.length, name: "Marce"}])
+  };  
+
+  var dataComponent = {
+    type: "button",
+    value: "Update",
+    hidden: false
+  }
+
+  //const[date, setDate] = useState(new Date());
+  const[seconds, setSeconds] = useState(0);
+  const[minutes, setMinutes] = useState(0);
+  const[hours, setHours] = useState(0);
+
+  useEffect (() => {
+    let interval = null;
+    interval = setInterval(() => {
+      while(seconds<60) {
+        setSeconds(seconds => seconds + 1);
+        if(seconds == 60) {
+          setSeconds(0);
+        }
+      }
+
+      while(minutes<60) {
+        setMinutes(minutes => minutes + 1);
+        if(minutes == 60) {
+          setMinutes(0);
+        }
+      }  
+
+      while(hours<24) {
+        setHours(hours => hours + 1);
+        if(hours == 24) {
+          setHours(0);
+        }
+      }
+    }, 1000);
+
+    return() => clearInterval(interval);
+    //var timerID = setInterval(() => tick(), 1000)
+    //return function cleanUp(params) {
+    //  clearInterval(timerID)
+    //}
+  });
+
+  //function tick() {
+  //  setDate(new Date())
+  //}
 
   return (
     <div>
-      <input type="button" onClick={onClick} value="Update" />
+      <h1>{hours}:{minutes}:{seconds}</h1>
+      <input onClick={onClick} {...dataComponent} />
       <div>
         {users.map((e) => (
           <div>
